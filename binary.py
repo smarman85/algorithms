0ce423b030da74316708d64801bca425dcbb2287
Sean <seanmarman@gmail.com>
2020-01-21 22:21:12 -0800

    first commit

---
+++ binary.py
@@ -0,0 +1,20 @@
+search = range(200)
+
+def isThere(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+
+        if guess == item:
+            return item
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+print(isThere(search, 53))
+print(isThere(search, 503))
